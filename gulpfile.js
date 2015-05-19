6790769c4bb0e895745f88b37d8753929e5dcbc3
mecum <rudy@onfroy.net>
2015-05-20 01:24:43 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var jshint      = require('gulp-jshint');
+
+gulp.task('minification', function () {
+    gulp.src(['dynamitable.jquery.js'])
+        .pipe(uglify())
+        .pipe(concat('dynamitable.jquery.min.js'))
+        .pipe(gulp.dest('./'))
+    ;
+});
+
+gulp.task('lint', function () {
+    gulp.src(['dynamitable.jquery.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter());
+});
+
+gulp.task('default', ['minification', 'lint']);
